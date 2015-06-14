c58ff8cd6f57fa91ab327928c7e2192e87f13633
findever <ifindever@gmail.com>
2015-06-14 20:46:07 +0800

    任务

--- js/common.js
+++ js/common.js
@@ -2,5 +2,28 @@
 
 // 侧滑菜单
 mui('.mui-table-view,.user-info,.tap-a').on('tap', 'a', function(e) {
-	window.location.href = this.getAttribute('href');
+	var href = this.getAttribute('href');
+	var target = this.getAttribute('target');
+	if (!href || href == "javascript:void(0)" || target) {
+		return;
+	}
+	window.location.href = href;
+});
+
+// 打开新webview
+mui.plusReady(function() {
+	mui('body').on('tap', 'a[target="_blank"]', function(e) {
+		var url = this.getAttribute('href');
+		var opt = {
+			url: url,
+			show: {
+				autoShow: true,//this.getAttribute("data-page-autoshow") ? true : false, //页面loaded事件发生后自动显示
+				duration: 300 //页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒；
+			}
+		}
+		if (this.getAttribute("data-page-id")) {
+			opt.id = this.getAttribute("data-page-autoshow");
+		}
+		mui.openWindow(opt);
+	});
 });
